fd4025b671adac455173febf9dbf07591fd3ed2c
Michael Schmid <michael.schmid@fhsg.ch>
2020-11-28 12:54:58 +0100

    Add files via upload

---
+++ data_cleaning_python/data_aggregation.py
@@ -0,0 +1,77 @@
+import pandas as pd
+
+# Load data:
+data = pd.read_csv("data/eignung_3_and_4_grouped.csv")
+data.drop(["Unnamed: 0"], axis=1, inplace=True)
+
+# Separation of the columns: Since we want one value per building,
+# we need to calculate sums and mean values, depending on the column.
+columns_means = ["X", "Y", "DACHART", "EXPO_GRAD", 
+                 "EXPO_KLASSE", "NEIGUNG", "EIGNUNG"]
+
+columns_sums = ["TD_FLAECHE", "GESAMT_EINSTR", "MITTLERE_EINSTR", 
+                "PV_ERTRAG", "PV_CO2_ERSP", "ST_ERTRAG", "ST_CO2_ERSP", 
+                "ST_OEL_ERSP"]
+
+# List of the columns we don't need the means or sums from (some
+# columns excluded)
+other_values = ["GWR_EGID", "Amtliche.Gebäudenummer", 
+                "Name.des.Gebäudes", "E.Gebäudekoordinate",
+                "N.Gebäudekoordinate", "Quartier", "Gebäudestatus",
+                "Gebäudekategorie", "Gebäudeklasse", 
+                "Baujahr.des.Gebäudes.YYYY", "Baumonat.des.Gebäudes.MM", 
+                "Baujahr..und.monat.des.Gebäudes..YYYY.oder.YYYY.MM..GBAUJ.GBAUM.",
+                "Bauperiode", "Renovationsjahr.des.Gebäudes",
+                "Abbruchjahr.des.Gebäudes", "Gebäudefläche", "Gebäudevolumen", 
+                "Informationsquelle.zum.Gebäudevolumen", "Gebäudevolumen..Norm",
+                "Anzahl.Geschosse", "Anzahl.separate.Wohnräume", "Zivilschutzraum",
+                "Energiebezugsfläche", "Wärmeerzeuger.Heizung.1",
+                "Energie..Wärmequelle.Heizung.1", "Informationsquelle.Heizung.1", 
+                "Datum.der.Erstellung", "Datum.der.letzten.Änderung", 
+                "BFS.Gemeindenummer", "Gemeindename", "Kantonskürzel"]
+
+# Complete list of other values: this list includes the columns that
+# are excluded in the list "other_values" if one wants to work with all
+# the columns.
+other_values2 = ["GWR_EGID", "Amtliche.Gebäudenummer", "Name.des.Gebäudes", 
+                 "E.Gebäudekoordinate", "N.Gebäudekoordinate", "Quartier",
+                 "Gebäudestatus", "Gebäudekategorie", "Gebäudeklasse",
+                 "Baujahr.des.Gebäudes.YYYY", "Baumonat.des.Gebäudes.MM",
+                 "Baujahr..und.monat.des.Gebäudes..YYYY.oder.YYYY.MM..GBAUJ.GBAUM.",
+                 "Bauperiode", "Renovationsjahr.des.Gebäudes", 
+                 "Abbruchjahr.des.Gebäudes", "Gebäudefläche", "Gebäudevolumen",
+                 "Informationsquelle.zum.Gebäudevolumen", "Gebäudevolumen..Norm",
+                 "Anzahl.Geschosse", "Anzahl.separate.Wohnräume", "Zivilschutzraum",
+                 "Energiebezugsfläche", "Wärmeerzeuger.Heizung.1",
+                 "Energie..Wärmequelle.Heizung.1", "Informationsquelle.Heizung.1", 
+                 "Aktualisierungsdatum.Heizung.1", "Wärmeerzeuger.Heizung.2",
+                 "Energie..Wärmequelle.Heizung.2", "Informationsquelle.Heizung.2",
+                 "Aktualisierungsdatum.Heizung.2", "Wärmeerzeuger.Warmwasser.1",
+                 "Energie..Wärmequelle.Warmwasser.1", "Informationsquelle.Warmwasser.1",
+                 "Aktualisierungsdatum.Warmwasser.1", "Wärmeerzeuger.Warmwasser.2",
+                 "Energie..Wärmequelle.Warmwasser..2", "Informationsquelle.Warmwasser.2",
+                 "Aktualisierungsdatum.Warmwasser.2", "Datum.der.Erstellung",
+                 "Datum.der.letzten.Änderung", "BFS.Gemeindenummer", "Gemeindename",
+                 "Kantonskürzel"]
+
+# Calculate means and sums:
+means = data.groupby(by="GEB_ID")[columns_means].mean()
+sums = data.groupby(by="GEB_ID")[columns_sums].sum()
+
+# Drop double rows from the "other_values" list (if applicable) 
+# and only keep the value from the corresponding first row:
+other_columns = data.groupby(by="GEB_ID")[other_values].first()
+
+# Concatenate the 3 dataframes:
+buildings = pd.concat([means, sums, other_columns], axis=1)
+
+# Add column "DACHART_Factor" and convert the "DACHART" floats into factors:
+buildings.loc[buildings["DACHART"] == 1, "DACHART_Factor"] = "slope"
+buildings.loc[buildings["DACHART"] == 0, "DACHART_Factor"] = "flat"
+buildings.loc[(buildings["DACHART"] > 0) & (buildings["DACHART"] < 1), "DACHART_Factor"] = "mixed"
+
+# Check the result:
+# buildings.head()
+
+# Write CSV file if desired:
+# buildings.to_csv("buildings.csv", index = True, header = True, sep = ',', encoding = 'utf-8')
---
+++ data_cleaning_python/finalizing.py
@@ -0,0 +1,128 @@
+import pandas as pd
+
+data = pd.read_csv("building-types-peak-power.csv")
+data.drop(["Unnamed: 0"], axis=1, inplace=True)
+
+# Create function to caluclate total floor area:
+def calculate_floor_area(area, floors):
+    """
+    :param area: base area of the building
+    :param floors: number of floors of the building
+    :return: total floor area of the building
+    """
+    return area * floors
+
+
+# Replace all NA values with 0:
+data.fillna(0, inplace=True)
+
+# Transformi columns from float to intetger:
+columns_to_transform = ["Bauperiode", "Renovationsjahr.des.Gebäudes", "Anzahl.Geschosse"]
+
+for column in columns_to_transform:
+    data.loc[:, column] = data.loc[:, column].astype(int)
+
+
+# Drop columns not needed:
+to_keep = ["GWR_EGID", "Peak Power", "Gebäudekategorie", 
+           "GBKLASSE", "Quartier", "TD_FLAECHE", "EIGNUNG", 
+           "Anzahl.Geschosse", "Gebäudefläche", "Bauperiode", 
+           "Renovationsjahr.des.Gebäudes"]
+
+data = data.loc[:, data.columns.intersection(to_keep)]
+
+
+
+# Mapping "Quartiere":
+neighborhoods = {1061001: "Oberseeburg-Rebstock",
+                 1061002: "Würzenbach-Schädrüti",
+                 1061003: "Bellerive-Schlössli",
+                 1061004: "Halde-Lützelmatt",
+                 1061005: "Wesemlin-Dreilinden",
+                 1061006: "Maihof-Rotsee",
+                 1061007: "Hochwacht-Zürichstrasse",
+                 1061008: "Altstadt-Wey",
+                 1061009: "Bramberg-St.Karli",
+                 1061010: "Kantonsspital-Ibach",
+                 1061011: "Baselstrasse-Bernstrasse",
+                 1061012: "Bruch-Gibraltar",
+                 1061013: "Obergütsch-Untergütsch",
+                 1061014: "Hirschmatt-Kleinstadt",
+                 1061015: "Obergrund-Allmend",
+                 1061016: "Neustadt-Voltastrasse",
+                 1061017: "Unterlachen-Tribschen",
+                 1061018: "Sternmatt-Hochrüti",
+                 1061019: "Langensand-Matthof",
+                 1061030: "Udelboden",
+                 1061031: "Reussbühl",
+                 1061032: "Ruopigen",
+                 1061033: "Matt",
+                 1061034: "Littau Dorf",
+                 1061035: "An der Emme",
+                 1061036: "Littauerberg",
+                 1061099: "Bürgenstock"}
+
+data.loc[:, "Quartier"] = data.loc[:, "Quartier"].map(neighborhoods)
+
+
+# Mapping "Bauperiode"
+bauperiode = {0: "Unbekannt",
+              8011: "Vor 1919", 
+              8012: "1919-1945",
+              8013: "1946-1960",
+              8014: "1961-1970",
+              8015: "1971-1980",
+              8016: "1981-1985",
+              8017: "1986-1990",
+              8018: "1991-1995",
+              8019: "1996-2000",
+              8020: "2001-2005",
+              8021: "2006-2010",
+              8022: "2011-2015",
+              8023: "Nach 2015"}
+
+data.loc[:, "Bauperiode"] = data.loc[:, "Bauperiode"].map(bauperiode)
+
+
+# Mapping building categories:
+building_categories = {1010: "Provisorische Unterkunft",
+                       1020: "Gebäude ausschließlich für Wohnnutzung",
+                       1030: "Wohngebäude mit Nebennutzung",
+                       1040: "Gebäude mit teilweiser Wohnnutzung",
+                       1060: "Gebäude ohne Wohnnutzung",
+                       1080: "Sonderbau"}
+
+data.loc[:, "Gebäudekategorie"] = data.loc[:, "Gebäudekategorie"].map(building_categories)
+
+
+# Add column with total floor area:
+area = data["Gebäudefläche"]
+floors = data["Anzahl.Geschosse"]
+
+data["Floor.Area"] = calculate_floor_area(area, floors)
+
+# Add address column:
+# Read data, concatenate address columns and add to the "addresses" df, drop "old" columns
+# and merge with the "data" df.
+
+addresses = pd.read_csv("addresses.csv")
+
+addresses["Adresse"] = addresses["Strassenbezeichnung"] + " " + addresses["Eingangsnummer Gebäude"]
+
+addresses.drop(["Strassenbezeichnung", "Eingangsnummer Gebäude"], axis=1, inplace=True)
+
+data = data.merge(addresses, left_on="GWR_EGID", right_on="GWR_EGID")
+
+# Rename columns
+data.rename(columns = {"GWR_EGID": "Eidgen. Gebäude ID",
+                       "TD_FLAECHE": "Dachfläche",
+                       "EIGNUNG": "Eignung",
+                       "Renovationsjahr.des.Gebäudes": "Renovationsjahr",
+                       "Anzahl.Geschosse": "Anzahl Geschosse",
+                       "GBKLASSE": "Gebäudeklasse",
+                       "Floor.Area": "Grundfläche"},
+           inplace=True)
+
+
+# Write CSV:
+data.to_csv("final_provisory.csv", index = False, header = True, sep = ',', encoding = 'utf-8')
